7004ddb865bb1569133eedf3e69a58cfcc0b000c
Henri Bouvier <henribouvier@yahoo.com>
2019-03-12 22:00:06 -0400

    refactoring

--- src/index.js
+++ src/index.js
@@ -1,35 +1,9 @@
 import './main.css';
 import { Elm } from './Main.elm';
-import config from './config';
 import registerServiceWorker from './registerServiceWorker';
 
-fetch(config.URL + "/latest", {
-    headers: {
-        "secret-key": config.SECRET
-    }
-})
-.then(res =>res.json())
-.then(json => {
-  const app = Elm.Main.init({
+Elm.Main.init({
     node: document.getElementById('root')
-  });
-  console.log('app:', app);
-
-
-
-
-    // app.ports.setStorage.subscribe(function(state) {
-    //   console.log('state:', state)
-    //     // fetch(config.URL, {
-    //     //     method: "PUT",
-    //     //     headers: {
-    //     //         "content-type": "application/json",
-    //     //         "secret-key": config.SECRET
-    //     //     },
-    //     //     body: JSON.stringify(state)
-    //     // });
-    // });
 });
-
 
 registerServiceWorker();
